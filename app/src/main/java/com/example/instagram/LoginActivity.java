bb377aec1682ab2fb8e4b47ab3d92116d6fcc0ff
agliyt <angiel@andrew.cmu.edu>
2021-07-06 16:26:42 -0400

    Initial commit - added app icon

---
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
